33da8a0c52f5ee83508bff2d736a4997d143eaa2
leo <lbwleon@qq.com>
2017-10-18 23:30:48 +0800

    ichat

--- src/main/java/xyz/isnull/blog/app/controller/ChatController.java
+++ src/main/java/xyz/isnull/blog/app/controller/ChatController.java
@@ -1,6 +1,7 @@
 package xyz.isnull.blog.app.controller;
 
 import com.alibaba.fastjson.JSON;
+import lombok.extern.log4j.Log4j;
 import nl.bitwalker.useragentutils.OperatingSystem;
 import nl.bitwalker.useragentutils.UserAgent;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import xyz.isnull.blog.app.model.ChatMessage;
 
@@ -16,6 +18,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Controller
+@Log4j
 public class ChatController {
     private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd HH:mm:ss");
 
@@ -28,14 +31,15 @@ public class ChatController {
     public String all(String message){
         ChatMessage chatMessage = JSON.parseObject(message, ChatMessage.class);
         chatMessage.setSendTime(simpleDateFormat.format(new Date()));
+        log.info(JSON.toJSONString(chatMessage));
         return JSON.toJSONString(chatMessage);
     }
 
     @RequestMapping("/ichat")
-    public String tochat(HttpServletRequest request){
+    public String tochat(HttpServletRequest request, ModelMap map){
         UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("User-Agent"));
         OperatingSystem os = userAgent.getOperatingSystem();
-        request.setAttribute("os",os.toString());
+        map.put("os",os.toString());
         return "chat/chat";
     }
 
